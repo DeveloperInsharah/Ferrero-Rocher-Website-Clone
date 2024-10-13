6fd925d3a7a2fc4a9a9856a8dd5c242cbbd52200
Developer Insharah <insharahattariya@gmail.com>
2024-10-13 07:35:18 -0700

    done

---
+++ app.js
@@ -0,0 +1,9 @@
+window.alert("Assalam-u-Alaikum I Am Developer Insharah I can make this Website Using HTML , CSS , Basic JAVASCRIPT And BOOTSTRAP.");
+window.alert("During Visit This Website PLease Click Navs Link I have A Surprise For You");
+window.alert("Please Make Sure Open Console At The End ");
+console.log("Hy 👋🏻");
+console.log("I AM DEVELOPER INSHARAH 👩🏻‍💻");
+console.log("I Study In Class 10 📖");
+console.log("My Roll No Is 7664 ");
+console.log("I can Make ⬅️ This Clone Of Ferrero Rocher Official Website");
+console.log("In ⬅️ This Clone I can use HTML , CSS , Baic Javascript AND BOOTSTRAP");
